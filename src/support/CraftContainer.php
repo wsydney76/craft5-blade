8702179df8c75a703da61a66404e58a52161fd43
wsydn <wsydney@web.de>
2025-12-19 15:59:02 +0100

    Update getNamespace method documentation in CraftContainer to clarify usage and provide alternative implementation guidance

--- src/support/CraftContainer.php
+++ src/support/CraftContainer.php
@@ -12,9 +12,19 @@ class CraftContainer extends Container
      */
     public function getNamespace(): string
     {
-        // Default Laravel apps use 'App\\' as the root namespace.
-        // Adjust if you want to resolve components from a different base.
-        // This comes into play when using custom directives like @datetime
+        /*
+         Default Laravel apps use 'App\\' as the root namespace.
+         Adjust if you want to resolve components from a different base.
+         This comes into play when using custom directives like @datetime or components like <x-...>
+         Note: This is a monkey patch, avoiding handling a full Laravel application just for this.
+
+         The 'correct' way would be use a full Application class extending Illuminate\Foundation\Application
+
+         composer require laravel/framework
+
+         use Illuminate\Foundation\Application;
+         $app = new Application(__DIR__);*/
+
         return 'App\\';
     }
 }
